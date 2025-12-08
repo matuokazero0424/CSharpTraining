89e85b7d740716077b3b7d32c404ba6cccbb2455
matsuokazero042456-admin <matuokazero0424@gmail.com>
2025-12-08 23:28:29 +0900

    値の入出力まで

--- Study/Study/Program.cs
+++ Study/Study/Program.cs
@@ -16,7 +16,25 @@ namespace Study
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Wolrd!!");
+            IntroduceMyself();
+        }
+        private static void SayFunc()
+        {
+            var word = Console.ReadLine();
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(word);
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+        }
+
+        private static void IntroduceMyself()
+        {
+            Console.WriteLine("アナタの名前は？");
+            var name = Console.ReadLine();
+            Console.WriteLine("アナタの年齢は？");
+            var age = int.Parse(Console.ReadLine());
+            Console.Write("アナタは{0}で、年齢は{1}歳です",name, age);
         }
     }
 }
